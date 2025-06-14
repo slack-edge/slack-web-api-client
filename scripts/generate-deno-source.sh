85c539d00f040b9af6af89b25f4e8c05230a3251
Kazuhiro Sera <seratch@gmail.com>
2025-06-14 19:03:31 +0900

    Fix the Deno source code generation script

--- scripts/generate-deno-source.sh
+++ scripts/generate-deno-source.sh
@@ -6,10 +6,12 @@ src_deno_dir=${script_dir}/../src_deno
 
 cp -p ${src_deno_dir}/deno.json . && \
   cp -p ${src_deno_dir}/README.md deno_README.md && \
+  cp -p ${src_deno_dir}/LICENSE.txt deno_LICENSE.txt && \
   rm -rf ${src_deno_dir}/* && \
   cp -pr ${src_dir}/* ${src_deno_dir}/ && \
   mv ./deno.json ${src_deno_dir}/ && \
   mv ./deno_README.md ${src_deno_dir}/README.md && \
+  mv ./deno_LICENSE.txt ${src_deno_dir}/LICENSE.txt && \
   cd ${script_dir} && \
   ruby ./deno_source_formatter.rb && \
   cd - && \
